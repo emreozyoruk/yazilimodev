2285b57c77c8497045dc10f4a83303ebd17cff12
cdemirtas <56775953+cdemirtas@users.noreply.github.com>
2023-05-29 21:08:02 +0300

    Update shome.php

--- shome.php
+++ shome.php
@@ -15,23 +15,23 @@ $name=$b['name'];
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Student Home</title>
+<title>Ogrenci Anasayfa</title>
 <link rel="stylesheet" type="text/css" href="scripts/styleASL.css" />
 </head>
 
 <body>
-<span class="head" style="float:left;">Welcome <span style="color:#F30;font-size:28px;"><?php echo $name;?></span></span>
+<span class="head" style="float:left;">Hosgeldiniz <span style="color:#F30;font-size:28px;"><?php echo $name;?></span></span>
 
-<span style="float:right;"><a href="logout.php">Logout</a></span><br />
+<span style="float:right;"><a href="logout.php">Cikis</a></span><br />
 
 <hr style="clear:both;box-shadow:0px 0px 2px #000;" color="#FF6600" size="2" width="100%"/><br />
 <div align="center">
 <table cellpadding="3" cellspacing="3" class="formTable">
 <tr><td>
-<span class="Subhead">Student Commands</span><hr size="1" style="color:#00F;" /><br />
-<a href="ViewNoticesStudent.php">View Notices</a><br />
-<a href="ChangePasswordStudent.php">Change Password</a><br />
-<a href="ComplaintStudent.php">Complaint Box</a><br />
+<span class="Subhead">Ogrenci Yorumlari</span><hr size="1" style="color:#00F;" /><br />
+<a href="ViewNoticesStudent.php">Notlari Görüntüle</a><br />
+<a href="ChangePasswordStudent.php">Şifre Değiştir</a><br />
+<a href="ComplaintStudent.php">Şikayet Kutusu</a><br />
 </td></tr></table>
 </div>
 </body>
