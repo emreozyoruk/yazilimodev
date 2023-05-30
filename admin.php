9d21c7d58ccaeb48b104d10fe4ef92731a75e860
Seras <72655616+emreozyoruk@users.noreply.github.com>
2023-05-30 23:59:31 +0300

    cigdemre burası full düzeltildi .

--- admin.php
+++ admin.php
@@ -10,24 +10,24 @@ if(!isset($_SESSION['admin']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Admin Home</title>
+<title>Admin Sayfası</title>
 <link href="scripts/styleASL.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-<span class="head" style="float:left">Welcome To Admin Panel</span>
+<span class="head" style="float:left">Admin Paneline Hoş Geldiniz</span>
 <span style="float:right;"><a href="logout.php">Cikis</a></span><br />
 
 <hr style="clear:both;box-shadow:0px 0px 2px #000;" color="#FF6600" size="2" width="100%"/><br />
 <div align="center">
 <table cellpadding="3" cellspacing="3" class="formTable">
 <tr><td>
-<span class="Subhead">Admin Commands</span><hr size="1" style="color:#00F;" /><br />
-<a href="notices.php">Online Notices</a><br />
-<a href="manageStudent.php">Manage Student Account</a><br />
-<a href="manageFaculty.php">Manage Faculty Account</a><br />
-<a href="ChangePassword.php">Sifreyi Degistir</a><br />
-<a href="Complaints.php">Manage Complaints</a><br />
+<span class="Subhead">Admin Komutları</span><hr size="1" style="color:#00F;" /><br />
+<a href="notices.php">Çevrimiçi Notlar</a><br />
+<a href="manageStudent.php">Öğrenci Hesaplarını Yönet</a><br />
+<a href="manageFaculty.php">Fakülte Hesaplarını Yönet</a><br />
+<a href="ChangePassword.php">Şifreyi Değiştir</a><br />
+<a href="Complaints.php">Şikayetleri Yönet</a><br />
 </td></tr></table>
 </div>
 
