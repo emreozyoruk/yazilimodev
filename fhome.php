d0fdaf2b871ffc782c142d1d27ba62f875d0ff6a
Seras <72655616+emreozyoruk@users.noreply.github.com>
2023-05-31 00:58:41 +0300

    full

--- fhome.php
+++ fhome.php
@@ -15,23 +15,23 @@ $name=$b['name'];
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Student Home</title>
+<title>Fakülte Sayfası</title>
 <link rel="stylesheet" type="text/css" href="scripts/styleASL.css" />
 </head>
 
 <body>
-<span class="head" style="float:left;">Welcome <span style="color:#F30;font-size:28px;"><?php echo $name;?></span></span>
+<span class="head" style="float:left;">Hoş Geldin <span style="color:#F30;font-size:28px;"><?php echo $name;?></span></span>
 
-<span style="float:right;"><a href="logout.php">Logout</a></span><br />
+<span style="float:right;"><a href="logout.php">Çıkış</a></span><br />
 
 <hr style="clear:both;box-shadow:0px 0px 2px #000;" color="#FF6600" size="2" width="100%"/><br />
 <div align="center">
 <table cellpadding="3" cellspacing="3" class="formTable">
 <tr><td>
-<span class="Subhead">Faculty Commands</span><hr size="1" style="color:#00F;" /><br />
-<a href="ViewNoticesFaculty.php">View Notices</a><br />
-<a href="ChangePasswordFaculty.php">Change Password</a><br />
-<a href="ComplaintFaculty.php">Complaint Box</a><br />
+<span class="Subhead">Fakülte Komutları</span><hr size="1" style="color:#00F;" /><br />
+<a href="ViewNoticesFaculty.php">Notları Görüntüle</a><br />
+<a href="ChangePasswordFaculty.php">Şifreyi Değiştir</a><br />
+<a href="ComplaintFaculty.php">Şikayet Sayfası</a><br />
 </td></tr></table>
 </div>
 </body>
